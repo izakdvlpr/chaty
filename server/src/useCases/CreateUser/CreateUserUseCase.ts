96ac2f997468fd5832c391af0c04980f655a18cf
zevdvlpr <zevdvlpr@gmail.com>
2020-09-14 17:15:42 -0300

    fix(server)

--- server/src/useCases/CreateUser/CreateUserUseCase.ts
+++
@@ -1,9 +0,0 @@
-import { ICreateUserRequestDTO } from './CreateUserDTO';
-
-export class CreateUserUseCase {
-  constructor(private usersRepository: any) {}
-
-  async execute(data: ICreateUserRequestDTO) {
-    console.log(data);
-  }
-}
--- server/src/useCases/CreateUser/index.ts
+++
@@ -1,8 +0,0 @@
-import { CreateUserUseCase } from './CreateUserUseCase';
-import { CreateUserController } from './CreateUserController';
-
-const createUserUseCase = new CreateUserUseCase(null);
-
-const createUserController = new CreateUserController(createUserUseCase);
-
-export { createUserController };

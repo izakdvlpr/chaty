9ebef17902a95dfc686dd22241583c44cd3aa07a
zevdvlpr <zevdvlpr@gmail.com>
2020-09-15 10:17:53 -0300

    chore(client): interfaces

---
+++ client/src/interfaces/index.tsx
@@ -0,0 +1,24 @@
+export interface ICreateUserDTO {
+  name: string;
+  email: string;
+  password: string;
+}
+
+export interface IUserAuthDTO {  
+  email: string;
+  password: string;
+}
+
+export interface IUserAuthResponse {  
+  id: string;
+  email: string;
+  token: string;
+}
+
+export interface IGetUserResponse {  
+  userId: string;  
+}
+
+export interface IDeleteUserDTO {  
+  email: string;
+}

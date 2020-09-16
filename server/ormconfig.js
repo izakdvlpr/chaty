6914ab1a197e7f740853ac9414a69b9fa9f3e20c
zevdvlpr <zevdvlpr@gmail.com>
2020-09-16 16:45:32 -0300

    feat(server): typerom config

--- server/ormconfig.js
+++ server/ormconfig.js
@@ -1,10 +1,10 @@
+if (process.env && process.env.NODE_ENV)
+  require('dotenv').config({ path: `./.env.${process.env.NODE_ENV}` });
+else require('dotenv').config({ path: './.env.development' });
+
 module.exports = {
   type: 'postgres',
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  url: process.env.DB_URL,
   entities: ['src/entities/*.ts'],
   migrations: ['src/database/migrations/*.ts'],
   cli: {

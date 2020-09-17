7699fe468d71ba64b83685da3472f353161c883a
zevdvlpr <zevdvlpr@gmail.com>
2020-09-16 22:54:48 -0300

    chore(client): app

---
+++ client/src/App.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { ThemeProvider } from "styled-components";
+
+import Routes from "./routes";
+
+import colors from "./styles/colors";
+import GlobalStyles from "./styles/global";
+
+function App() {
+  return (
+    <ThemeProvider theme={colors}>
+      <GlobalStyles />
+
+      <Routes />
+    </ThemeProvider>
+  );
+}
+
+export default App;

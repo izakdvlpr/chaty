7f79fd5a51dac78c223116a093163210e036734c
zevdvlpr <zevdvlpr@gmail.com>
2020-09-16 22:51:55 -0300

    chore(client): components

---
+++ client/src/components/Button/index.tsx
@@ -0,0 +1,11 @@
+import React, { ButtonHTMLAttributes } from 'react';
+
+import { Container } from './styles';
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+
+const Button: React.FC<ButtonProps> = ({ children }) => {
+  return <Container>{children}</Container>;
+};
+
+export default Button;

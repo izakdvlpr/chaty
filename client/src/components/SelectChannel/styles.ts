b7f7a2d787a462a84f732a974a7dcdc9f8544280
zevdvlpr <zevdvlpr@gmail.com>
2020-09-17 21:18:51 -0300

    chore(client): select channel

---
+++ client/src/components/SelectChannel/styles.ts
@@ -0,0 +1,191 @@
+import styled from 'styled-components';
+import { shade, lighten } from 'polished';
+
+import { FaUserFriends } from 'react-icons/fa';
+import { MdAdd, MdClose } from 'react-icons/md';
+
+export const Container = styled.div`
+  grid-area: SC;
+
+  padding: 8px;
+
+  background-color: ${({ theme }) => shade(0.1, theme.background)};
+
+  overflow-y: scroll;
+
+  ::-webkit-scrollbar {
+    width: 4px;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    &:hover {
+      border-radius: 4px;
+      background-color: ${({ theme }) => shade(0.4, theme.background)};
+    }
+  }
+  ::-webkit-scrollbar-track {
+    background-color: ${({ theme }) => shade(0.1, theme.background)};
+  }
+`;
+
+export const FriendsContent = styled.div`
+  width: 100%;
+  height: 45px;
+
+  margin: 3px 0;
+  padding: 0 8px;
+
+  display: flex;
+  align-items: center;
+
+  font-size: 16px;
+  font-weight: 400;
+  color: ${({ theme }) => theme.text};
+
+  border-radius: 4px;
+
+  cursor: pointer;
+
+  transition: background-color 0.2s;
+  background-color: transparent;
+
+  &:hover,
+  &.active {
+    color: ${({ theme }) => lighten(0.25, theme.text)};
+
+    background-color: ${({ theme }) => lighten(0.015, theme.background)};
+
+    svg {
+      fill: ${({ theme }) => lighten(0.25, theme.text)};
+    }
+  }
+`;
+
+export const FriendsIcon = styled(FaUserFriends)`
+  width: 24px;
+  height: 24px;
+
+  margin-right: 10px;
+
+  fill: ${({ theme }) => theme.text};
+`;
+
+export const Label = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  margin-top: 20px;
+  padding: 0 8px;
+
+  text-transform: uppercase;
+  font-size: 11px;
+  font-weight: 700;
+  color: ${({ theme }) => theme.text};
+`;
+
+export const CreateIcon = styled(MdAdd)`
+  width: 20px;
+  height: 20px;
+
+  fill: ${({ theme }) => theme.text};
+
+  cursor: pointer;
+
+  &:hover {
+    fill: ${({ theme }) => lighten(0.25, theme.text)};
+  }
+`;
+
+export const List = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  margin-top: 10px;
+`;
+
+export const Group = styled.div`
+  width: 100%;
+  height: 45px;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  margin: 3px 0;
+  padding: 0 8px;
+
+  cursor: pointer;
+
+  transition: background-color 0.2s;
+  background-color: transparent;
+
+  &:hover,
+  &.active {
+    h4 {
+      color: ${({ theme }) => lighten(0.25, theme.text)};
+    }
+
+    background-color: ${({ theme }) => lighten(0.015, theme.background)};
+  }
+`;
+
+export const Profile = styled.div`
+  display: flex;
+  align-items: center;
+
+  > h4 {
+    font-weight: 500;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    overflow: hidden;
+    color: ${({ theme }) => theme.text};
+  }
+`;
+
+export const AvatarWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  flex-shrink: 0;
+
+  margin-right: 12px;
+
+  position: relative;
+
+  &::after {
+    height: 10px;
+    width: 10px;
+
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    
+    border-radius: 50%;
+
+    content: '';
+
+    background-color: ${({ theme }) => theme.idle};
+  }
+`;
+
+export const Avatar = styled.img`
+  width: 35px;
+  height: 35px;
+
+  border-radius: 50%;
+`;
+
+export const CloseIcon = styled(MdClose)`
+  width: 20px;
+  height: 20px;
+
+  fill: ${({ theme }) => theme.text};
+
+  cursor: pointer;
+
+  &:hover {
+    fill: ${({ theme }) => lighten(0.25, theme.text)};
+  }
+`;

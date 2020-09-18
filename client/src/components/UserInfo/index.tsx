7521da980cddb8de5b9e1dc20c7b504f15fbf927
zevdvlpr <zevdvlpr@gmail.com>
2020-09-17 21:19:06 -0300

    chore(client): user info

---
+++ client/src/components/UserInfo/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import {
+  Container,
+  Profile,
+  AvatarWrapper,
+  Avatar,
+  UserData,
+  Icons,
+  LogoutIcon,
+  SettingsIcon,
+} from './styles';
+
+const UserInfo: React.FC = () => {
+  return (
+    <Container>
+      <Profile>
+        <AvatarWrapper>
+          <Avatar src="https://github.com/zevdvlpr.png" alt="avatar" />
+        </AvatarWrapper>
+
+        <UserData>
+          <strong>zevdvlpr</strong>
+          <span>#7777</span>
+        </UserData>
+      </Profile>
+
+      <Icons>
+        <Link to="/logout">          
+          <LogoutIcon />
+        </Link>
+        <SettingsIcon />
+      </Icons>
+    </Container>
+  );
+};
+
+export default UserInfo;

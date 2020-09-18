3691e3d5f76c310e1b7151b1fbef58a383eb761b
zevdvlpr <zevdvlpr@gmail.com>
2020-09-17 21:19:17 -0300

    chore(client): user list

---
+++ client/src/components/UserList/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const UserList: React.FC = () => {
+  return (
+    <Container>
+      {/* <h1>UserList</h1> */}
+    </Container>
+  );
+};
+
+export default UserList;

7e4f4d0fdf168a203ed17adb1e2046caa073d18b
zevdvlpr <zevdvlpr@gmail.com>
2020-09-17 21:14:34 -0300

    chore(client): new pages

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -3,7 +3,9 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Home from '../pages/Home';
 import Login from '../pages/Login';
+import Logout from '../pages/Logout';
 import Register from '../pages/Register';
+import Profile from '../pages/Profile';
 
 function Routes() {
   return (
@@ -11,7 +13,9 @@ function Routes() {
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/login" component={Login} />
-        <Route path="/register" component={Register} />
+        <Route path="/logout" component={Logout} />
+        <Route path="/register" component={Register} />        
+        <Route path="/@me" component={Profile} />
       </Switch>
     </BrowserRouter>
   );
